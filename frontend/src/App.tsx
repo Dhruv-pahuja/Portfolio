a9059b6576b7395694ce632f0aea6e53834a1e95
Dhruv Pahuja <dhruvpahuja013@gmail.com>
2024-12-08 21:54:10 +0530

    added dark mode toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
+import React from 'react';
+import {Layout} from "./components/index"
 
-
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-      <h1 className='text-xl'>welcome</h1>
-    </>
-  )
-}
-
+    <Layout>
+      <div className="p-2  dark:text-white">
+        <h1 className="text-2xl dark:text-white">Welcome to My Portfolio</h1>
+        <p className="text-gray-700 dark:text-gray-300 ">
+          This is a single-page portfolio showcasing my projects and skills.
+        </p>
+      </div>
+    </Layout>
+  );
+};
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {Provider} from 'react-redux'
+import store from '../src/store/store.ts'
 import './index.css'
 import App from './App.tsx'
 
+const currentTheme = store.getState().theme.theme;
+document.documentElement.className = currentTheme
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
