e1681d6344e584713f4f88e6c24d3c362b8dd868
Dhruv Pahuja <dhruvpahuja013@gmail.com>
2024-12-18 22:55:41 +0530

    work in progess in Project component

--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -11,32 +11,32 @@ type Project = {
 };
 
 const projects: Project[] = [
-  {
-    id: 1,
-    title: "DevForum",
-    description:
-      "DevForum allows developers to ask questions, provide answers, and engage in discussions within a community-driven platform.",
-    image: "/path/to/devforum.jpg",
-    tags: ["NEXT.JS", "MONGODB", "TAILWIND CSS", "TYPESCRIPT", "SHADCN", "CLERK"],
-    link: "https://example.com/devforum",
-  },
-  {
-    id: 2,
-    title: "ViNEXT",
-    description:
-      "Video conferencing web application with a focus on high-quality video streaming and low-latency communication.",
-    image: "/path/to/vinext.jpg",
-    tags: ["NEXT.JS", "SHADCN", "CLERK", "STREAM", "TYPESCRIPT", "TAILWINDCSS"],
-    link: "https://example.com/vinext",
-  },
-  {
-    id: 3,
-    title: "Full Stack Twitter Clone",
-    description: "Twitter clone using modern technologies.",
-    image: "/path/to/twitter-clone.jpg",
-    tags: ["REACT.JS", "NODE.JS", "EXPRESS.JS", "MONGODB", "RESTFUL API", "CLOUDINARY"],
-    link: "https://example.com/twitter-clone",
-  },
+  // {
+  //   id: 1,
+  //   title: "DevForum",
+  //   description:
+  //     "DevForum allows developers to ask questions, provide answers, and engage in discussions within a community-driven platform.",
+  //   image: "/path/to/devforum.jpg",
+  //   tags: ["NEXT.JS", "MONGODB", "TAILWIND CSS", "TYPESCRIPT", "SHADCN", "CLERK"],
+  //   link: "https://example.com/devforum",
+  // },
+  // {
+  //   id: 2,
+  //   title: "ViNEXT",
+  //   description:
+  //     "Video conferencing web application with a focus on high-quality video streaming and low-latency communication.",
+  //   image: "/path/to/vinext.jpg",
+  //   tags: ["NEXT.JS", "SHADCN", "CLERK", "STREAM", "TYPESCRIPT", "TAILWINDCSS"],
+  //   link: "https://example.com/vinext",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Full Stack Twitter Clone",
+  //   description: "Twitter clone using modern technologies.",
+  //   image: "/path/to/twitter-clone.jpg",
+  //   tags: ["REACT.JS", "NODE.JS", "EXPRESS.JS", "MONGODB", "RESTFUL API", "CLOUDINARY"],
+  //   link: "https://example.com/twitter-clone",
+  // },
 ];
 
 const Projects: React.FC = () => {
@@ -45,6 +45,9 @@ const Projects: React.FC = () => {
       <div className="max-w-7xl mx-auto px-5">
         <h2 className="text-3xl font-semibold text-gray-800 dark:text-white mb-6">
           My Work
+
+          <p className="text-center align-middle text-gray-400">
+            */working on this component/*</p>
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {projects.map((project) => (
@@ -53,7 +56,7 @@ const Projects: React.FC = () => {
               href={project.link}
               target="_blank"
               rel="noopener noreferrer"
-              className="block rounded-lg overflow-hidden shadow-lg bg-white dark:bg-gray-900 transform transition-transform duration-300 hover:cursor-pointer hover:scale-105 hover:shadow-2xl"
+              className="block rounded-lg overflow-hidden shadow-lg bg-white dark:bg-black dark:border transform transition-transform duration-300 hover:cursor-pointer hover:scale-105 hover:shadow-2xl"
             >
               <img
                 src={project.image}
